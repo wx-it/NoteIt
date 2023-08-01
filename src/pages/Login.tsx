61f428cc606b2a38d93b15ef7a69b1023984d016
Lix <zinebbendjafer9@gmail.com>
2023-08-01 20:17:13 +0100

    styling login form done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,10 +3,10 @@ import loginImg from "../assets/img/login.jpg";
 
 const Login = () => {
   return (
-    <div className="flex w-full h-screen items-center justify-between">
+    <div className="flex w-full items-start justify-between">
       <LoginForm />
-      <div className="w-[50%] h-screen object-cover md:visible">
-        <img src={loginImg} alt="" />
+      <div className="w-[50%] h-screen md:visible">
+        <img className=" h-full w-full object-cover filter brightness-85 " src={loginImg} alt="" />
       </div>
     </div>
   );
